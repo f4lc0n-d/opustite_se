41150b1dd36046a0a274b19ad76f0d133e87f4e1
f4lc0n-d <56363580+f4lc0n-d@users.noreply.github.com>
2024-03-21 08:57:57 +0100

    napravljen main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import time
+
+print('Rale je objasnio par stvari i mora da nauci da koristi python')
+
+time.sleep(2)
+
+broj = float(input('Izaberi broj: ')
+
+load1 = 'kompjuter razmislja'
+laod2 = 'kompjuter razmislja.'
+load3 = 'kompjuter razmislja..'
+load4 = 'kompjuter razmislja...'
+
+print(load1)
+time.sleep(0.5)
+print(load2)
+time.sleep(0.5)
+print(load3)
+time.sleep(0.5)
+
+print('opusti se rale')
